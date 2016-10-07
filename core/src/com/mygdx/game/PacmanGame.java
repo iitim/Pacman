5aa1338bab000d34fba6c52b6e4f676083141ccb
Chayanin Tongphasook <chayanin2007@gmail.com>
2016-10-07 11:55:04 +0700

    control pacman

--- core/src/com/mygdx/game/PacmanGame.java
+++ core/src/com/mygdx/game/PacmanGame.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PacmanGame extends Game {
-	public SpriteBatch batch;
+	public SpriteBatch batch; //SpriteBatch is show all drawing in 1 time.
 	
 	@Override
 	public void create () {
