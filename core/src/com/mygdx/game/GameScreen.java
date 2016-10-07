1b5ced49e2477a8bcc1f5e51d68b29e01defe3b3
Chayanin Tongphasook <chayanin2007@gmail.com>
2016-10-07 12:22:16 +0700

    class pacman: direction

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -6,44 +6,53 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
  
 public class GameScreen extends ScreenAdapter {
 
     private PacmanGame pacmanGame;
     private Texture pacmanImg;
-    private int x;
-    private int y;
+    private Pacman pacman;
     
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
+        pacman = new Pacman(100,100);
     }
     
     @Override
     public void render(float delta) {
+//        world.update(delta);
+//        
+//        Gdx.gl.glClearColor(0, 0, 0, 1);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clean screen
+// 
+//        worldRenderer.render();
+        
+        update(delta);
+        
     	Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clean screen
-        update(delta);
+
     	SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, x, y);
-        batch.end();   
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
+        batch.end();
     }
     
     private void update(float delta) {
     	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+    		pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+        	pacman.move(Pacman.DIRECTION_RIGHT);
         }  
         if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-            y -= 10;
+        	pacman.move(Pacman.DIRECTION_DOWN);
         }
         if(Gdx.input.isKeyPressed(Keys.UP)) {
-            y += 10;
+        	pacman.move(Pacman.DIRECTION_UP);
         }
     }
 }
