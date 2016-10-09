162fc0a548138420a6251aa004e7ff68b225c6af
Chayanin Tongphasook <chayanin2007@gmail.com>
2016-10-09 12:32:30 +0700

    class WorldRenderer

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -4,41 +4,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
  
 public class GameScreen extends ScreenAdapter {
 
     private PacmanGame pacmanGame;
-    private Texture pacmanImg;
+//    private Texture pacmanImg;
     World world;
+    WorldRenderer worldRenderer;
     
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
-        pacmanImg = new Texture("pacman.png");
+//        pacmanImg = new Texture("pacman.png");
         world = new World(pacmanGame);
     }
     
     @Override
     public void render(float delta) {
 //        world.update(delta);
-//        
-//        Gdx.gl.glClearColor(0, 0, 0, 1);
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clean screen
-// 
-//        worldRenderer.render();
-        
         update(delta);
         
     	Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clean screen
 
-    	SpriteBatch batch = pacmanGame.batch;
-        batch.begin();
-        Vector2 pos = world.getPacman().getPosition();
-        batch.draw(pacmanImg, pos.x, pos.y);
-        batch.end();
+        worldRenderer = new WorldRenderer(pacmanGame,world);
+        worldRenderer.render(delta);
     }
     
     private void update(float delta) {
