b0eb5ddcd2aefffecb7d60df032476ebeb2f5804
Chayanin Tongphasook <chayanin2007@gmail.com>
2016-10-09 10:44:56 +0700

    class pacman

--- core/src/com/mygdx/game/Pacman.java
+++ core/src/com/mygdx/game/Pacman.java
@@ -10,6 +10,8 @@ public class Pacman {
     public static final int DIRECTION_DOWN = 3;
     public static final int DIRECTION_LEFT = 4;
     public static final int DIRECTION_STILL = 0;
+    
+    public static final int SPEED = 10;
     private static final int [][] DIR_OFFSETS = new int [][] {
         {0,0},
         {0,-1},
@@ -41,7 +43,7 @@ public class Pacman {
 //            position.x -= 10;
 //            break;
 //        }
-    	position.x += 10 * DIR_OFFSETS[dir][0];
-        position.y += 10 * DIR_OFFSETS[dir][1];
+    	position.x += SPEED * DIR_OFFSETS[dir][0];
+        position.y += SPEED * DIR_OFFSETS[dir][1];
     }
 }
